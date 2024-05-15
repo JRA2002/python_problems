c908c34bdc964412e00c8417c48bca2263637ebb
javier <javier_2790@outlook.com>
2024-05-15 16:45:09 +0200

    f

---
+++ WPM_test.py
@@ -0,0 +1,81 @@
+import curses
+from curses import wrapper
+import os
+import time
+
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr('welcome to wpm test speed for you')
+    stdscr.addstr('\nPress any key to begin')
+    stdscr.refresh()
+    stdscr.getkey()
+
+def display_text(stdscr,target,current,wpm=0):
+    stdscr.addstr(target)
+    stdscr.addstr(2,0,f'WPM = {wpm}')
+    
+    for i,char in enumerate(current):
+        correct_char = target[i]
+        color = curses.color_pair(3)
+
+        if char != correct_char:
+            color = curses.color_pair(2)
+        stdscr.addstr(0,i,char, color)
+
+
+
+
+def wpm_test(stdscr):
+    target_text = 'Hello this is a wpm test for you'
+    current_text = []
+    wpm = 0
+    stdscr.nodelay(True)
+    start_time = time.time()
+
+    while True:
+
+        time_elapsed = max(time.time() - start_time,1)
+        wpm = round((len(current_text) / (time_elapsed/60)) / 5)
+
+        stdscr.clear()
+        display_text(stdscr,target_text,current_text,wpm)
+        stdscr.refresh()
+
+        if ''.join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
+
+        if ord(key) == 27:
+            break
+        if key in ('KEY_BACKSPACE','\b','\x7f'):
+             if len(current_text) > 0:
+                current_text.pop()
+        elif len(current_text) < len(target_text):
+             current_text.append(key)
+    
+        
+
+def main(stdscr):
+    curses.init_pair(1,curses.COLOR_BLUE,curses.COLOR_BLACK)
+    curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
+    curses.init_pair(3,curses.COLOR_GREEN,curses.COLOR_BLACK)
+
+    start_screen(stdscr)
+    
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(4,0,'You completed the test....GOOD JOB..Press any key to continue..')
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+wrapper(main)
+#os.system('clear')
+
