3f77279b7a2193014f45443127e39f6b86a255ee
javier <javier_2790@outlook.com>
2024-07-22 23:17:25 +0200

    feat: add palndrome

--- recurvise/palindrome.py
+++ recurvise/palindrome.py
@@ -1,11 +1,47 @@
-def palindrome(s, i):
-    if len(s) == 1:
-        return True
-    else:
-        if s[0] == s[len(s) - 1]:
-            return True
-        else:
-            palindrome(s[i+1:len(s)-2])
+# A recursive Python program 
+# to check whether a given 
+# number is palindrome or not
 
-res = palindrome('alddola', 0)
-print(res)
+# A recursive function that 
+# check a str[s..e] is 
+# palindrome or not.
+def isPalRec(st, s, e) :
+	
+	# If there is only one character
+	if (s == e):
+		return True
+
+	# If first and last
+	# characters do not match
+	if (st[s] != st[e]) :
+		return False
+
+	# If there are more than 
+	# two characters, check if 
+	# middle substring is also 
+	# palindrome or not.
+	if (s < e + 1) :
+		return isPalRec(st, s + 1, e - 1)
+
+	return True
+
+def isPalindrome(st) :
+	n = len(st)
+	
+	# An empty string is 
+	# considered as palindrome
+	if (n == 0) :
+		return True
+	
+	return isPalRec(st, 0, n - 1)
+
+
+# Driver Code
+st = "geeg"
+if (isPalindrome(st)) :
+	print("Yes")
+else :
+	print("No")
+	
+# This code is contributed
+# by Nikita Tiwari.
