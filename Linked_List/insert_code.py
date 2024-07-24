75c77e1275de02d87b729335ba3bcadc11edeec2
javier <javier_2790@outlook.com>
2024-07-24 21:09:50 +0200

    feat:add more problems LList

--- Linked_List/insert_code.py
+++ Linked_List/insert_code.py
@@ -4,5 +4,139 @@ class Node:
         self.data = data
         self.next = None
 # insert a new node
-def pushNode()
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insertNode(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        
+    def insertAtIndex(self, data, index):
+        if index == 0:
+            self.insertNode(data)
+        position = 0
+        current_node = self.head
+        while (current_node != None) and (position + 1 != index):
+            position += 1
+            current_node =current_node.next
+
+        if current_node != None:
+            new_node = Node(data)
+            new_node.next = current_node.next
+            current_node.next = new_node
+        else:
+            print('not index present')
+
+    def removeFirstNode(self):
+        if self.head is None:
+            return
+        else:
+            self.head = self.head.next
+
+    def insertAtEnd(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    def updateNode(self, val, index):
+        position = 0
+        current_node = self.head
+
+        if position == index:
+            current_node.data = val
+        else:
+            while position != index and (current_node != None):
+                position += 1
+                current_node = current_node.next
+            if current_node != None:
+                current_node.data = val
+            else:
+                print('Index not present')
+
+    def removeLastNode(self):
+        if self.head is None:
+            return
+        current_node = self.head
+        while current_node != None and (current_node.next.next != None):
+            current_node = current_node.next
+        current_node.next = None
+    
+    def remove_at_index(self, index):
+        if self.head is None:
+            return
+
+        position = 0
+        current_node = self.head
+        if position == index:
+            self.removeFirstNode()
+        else:
+            while current_node != None and (position + 1 < index):
+                position += 1
+                current_node = current_node.next
+            if current_node != None:
+                current_node.next = current_node.next.next
+            else:
+                print('Index Not present')
+            
+    def remove_node_by_value(self, data):
+        current_node = self.head
+        if current_node.data == data:
+            self.removeFirstNode()
+            return
+        else:
+            while current_node != None and current_node.next.data != data:
+                current_node = current_node.next
+            if current_node == None:
+                return
+            else:
+                current_node.next = current_node.next.next
+    def size_llist(self):
+        size = 0
+        if self.head:
+            current_node = self.head
+            while current_node:
+                size += 1
+                current_node = current_node.next
+            return size
+        else:
+            return 0
+                    
+                    
+
+    def printLL(self):
+        current_node = self.head
+        while(current_node):
+            print(current_node.data)
+            current_node = current_node.next
+        
+
+
+llist = LinkedList()
+llist.insertNode(2)
+llist.insertNode(3)
+llist.insertNode(4)
+llist.insertAtIndex(5, 2)
+llist.updateNode('a', 0)
+
+print("Node Data")
+llist.printLL()
+print("size of linked list")
+res = llist.size_llist()
+print(res)
+
+
+
+
+        
         
