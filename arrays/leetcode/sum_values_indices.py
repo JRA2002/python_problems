7af1b468e7617178ede462329693160c56c1bc15
javier <javier_2790@outlook.com>
2024-09-02 11:43:06 +0200

    feat: add leetcode problems

--- arrays/leetcode/sum_values_indices.py
+++ arrays/leetcode/sum_values_indices.py
@@ -8,9 +8,26 @@ The set bits in an integer are the 1's present when it is written in binary.
 For example, the binary representation of 21 is 10101, which has 3 set bits.'''
 
 def binary_indices(nums, k):
-    pass
+    def number_bits(num, k):
+            count = 0
+            while num > 0:
+                count += num & 1
+                num >>= 1
+                if count > k:
+                    return False
+            if count == k:
+                return True
+                
+    suma = 0
+    for num in range(len(nums)):
+        res = number_bits(num, k)
+        if res:
+            suma += nums[num]
+    return suma
 
-nums = [5,10,1,5,2]
-k = 1
-res = binary_indices(nums, k)
-print(res)
+
+nums = []
+nums.insert(0, 0)
+nums.insert(1, 2)
+nums.insert(1, 5)
+print(nums)
