b59c133c1df625927fbbeb98454b1635a8a6f985
javier <javier_2790@outlook.com>
2024-10-05 09:57:37 +0200

    feat: add more

---
+++ arrays/leetcode/easy/array_form.py
@@ -0,0 +1,13 @@
+'''The array-form of an integer num is an array representing its digits in left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.'''
+
+def array_form(num, k):
+    pass
+
+num = [1,2,0,0]
+k = 34
+
+res = array_form(num, k)
+print(res)
