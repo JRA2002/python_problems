43fa4466ed5df7c709d80c63d93d31d6731a5411
javier <javier_2790@outlook.com>
2024-11-12 22:25:07 +0100

    feat: add leetcode

---
+++ GEEKFORGEEKS/medium/maximum_index.py
@@ -0,0 +1,10 @@
+'''Given an array arr of positive integers. The task is to return the maximum of j - i subjected to the constraint of arr[i] < arr[j] and i < j.'''
+
+def maximum_index(arr: list):
+
+    pass
+
+arr = [34, 8, 10, 3, 2, 80, 30, 33, 1]
+
+res = maximum_index(arr)
+print(res)
