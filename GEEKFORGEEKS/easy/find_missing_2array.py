e03ef914730d68eeacffe03e671c43c72e6ee32f
javier <javier_2790@outlook.com>
2024-11-14 00:26:36 +0100

    feat: add more GFG problems

--- GEEKFORGEEKS/easy/find_missing_2array.py
+++ GEEKFORGEEKS/easy/find_missing_2array.py
@@ -2,10 +2,22 @@
 
 Note: Numbers to be returned should in order as they appear in array a'''
 
-def find_missing(a,b,m,n):
-    pass
+def find_missing(a,b,n,m):
+    hashmap = {}
+    ans = []
+    for num in b:
+        if num not in hashmap:
+            hashmap[num] = 1
+    
+    for num in a:
+        if num not in hashmap:
+            ans.append(num)
+    return ans
 
 a = [1, 2, 3, 4, 5, 10]
 n = len(a)
 b = [2, 3, 1, 0, 5]
 m = len(b)
+
+res = find_missing(a, b, n, m)
+print(res)
--- GEEKFORGEEKS/medium/maximum_index.py
+++ GEEKFORGEEKS/medium/maximum_index.py
@@ -2,7 +2,13 @@
 
 def maximum_index(arr: list):
 
-    pass
+    lmin = []
+    rmax = []
+    n = len(arr)
+    mini = arr[0]
+    for i in range(n//2 + 1):
+        if arr[i] < mini:
+            mini
 
 arr = [34, 8, 10, 3, 2, 80, 30, 33, 1]
 
