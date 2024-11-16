a627e327830af9a8cd7590927dc2122577288a86
javier <javier_2790@outlook.com>
2024-11-16 21:10:31 +0100

    feat: add more GFG

---
+++ GEEKFORGEEKS/easy/modify_array.py
@@ -0,0 +1,36 @@
+'''Given an array arr. Return the modified array in such a way that if the current and next numbers are valid numbers and are equal then double the current number value and replace the next number with 0. After the modification, rearrange the array such that all 0's are shifted to the end.
+
+Note:
+
+Assume ‘0’ as the invalid number and all others as a valid number.
+The sequence of the valid numbers is present in the same order.'''
+
+def modify_array(arr: list):
+    n = len(arr)
+    for i in range(n - 1):
+        if arr[i] == arr[i+1]:
+            arr[i] = arr[i]*2
+            arr[i+1] = 0
+    print(arr)
+    i = 0
+    j = i + 1
+    print(i,j)
+    while i < j and j < n and i < n:
+        print(i,j)
+        if arr[i] == 0 and arr[j] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j += 1
+           
+        if i+1<n and j+1<n:
+            if arr[i] != 0:
+                i += 1
+            if arr[j] == 0:
+                j += 1
+
+    return arr
+
+arr = [14, 13, 13, 20, 20, 13, 19, 11, 18, 17, 17, 11, 17, 15, 13, 13, 10, 12, 19]
+
+res = modify_array(arr)
+print(res)
--- GEEKFORGEEKS/easy/swap_elements.py
+++ GEEKFORGEEKS/easy/swap_elements.py
@@ -1,8 +1,10 @@
 '''Given an array arr of n positive integers. The task is to swap every ith element of the array with (i+2)th element.'''
 
 def swap_elements(arr: list, n):
-    pass
-
+    for i in range(n-2):
+        arr[i], arr[i+2] = arr[i+2], arr[i]
+        print(arr)
+    return arr
 arr = [1,2,3,4,5]
 n = len(arr)
 
