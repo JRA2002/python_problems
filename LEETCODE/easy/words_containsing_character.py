d80c149d990ed3fb08b715df6d2558ba38a7d22a
javier <javier_2790@outlook.com>
2024-11-23 20:59:36 +0100

    feat: add leetcode

--- LEETCODE/easy/words_containsing_character.py
+++ LEETCODE/easy/words_containsing_character.py
@@ -5,7 +5,16 @@ Return an array of indices representing the words that contain the character x.
 Note that the returned array may be in any order.'''
 
 def find_words(words: list, x: str):
-    pass
+    res = []
+    i = 0
+    for word in words:
+        for char in word:
+            if char == x:
+                res.append(i)
+                break
+        i += 1
+
+    return res
 
 words = ["leet","code"]
 x = "e"
